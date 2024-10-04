4e499eff44deab3cb16514977aaad43b3cbdcb3b
Sanchari Mandal <113378204+Hul0@users.noreply.github.com>
2024-10-04 14:15:59 +0530

    Add onSubmit event to LoginForm

--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useAuth } from "@/context/AuthContext";
-import React, { useState, FormEvent } from "react";
+import React, { FormEvent } from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import {
@@ -13,7 +13,13 @@ import {
 import { useRouter } from "next/navigation";
 import { Label } from "../ui/label";
 import Link from "next/link";
-import { Card, CardContent, CardHeader } from "../ui/card";
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardDescription,
+    CardTitle,
+} from "../ui/card";
 
 const LoginForm: React.FC = () => {
     const { login } = useAuth();
@@ -40,141 +46,75 @@ const LoginForm: React.FC = () => {
     };
 
     return (
-        <Card className="mx-auto max-w-sm bg-slate-950/20">
-            <CardHeader>
-                <CardTitle className="text-2xl">Login</CardTitle>
-                <CardDescription>
-                    Enter your email below to login to your account
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <div className="grid gap-4">
-                    <div className="grid gap-2">
-                        <Label htmlFor="email">Email</Label>
-                        <Input
-                            id="email"
-                            type="email"
-                            name="email"
-                            placeholder="m@example.com"
-                            required
-                        />
-                    </div>
-                    <div className="grid gap-2">
-                        <div className="flex items-center">
-                            <Label htmlFor="password">Password</Label>
-                            <Link
-                                href="/404"
-                                className="ml-auto inline-block text-sm underline"
-                            >
-                                Forgot your password?
-                            </Link>
+        <form onSubmit={handleSubmit}>
+            <Card className="mx-auto max-w-sm bg-slate-950/20">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                name="email"
+                                placeholder="m@example.com"
+                                required
+                            />
                         </div>
-                        <Input
-                            id="password"
-                            type="password"
-                            name="password"
-                            required
-                        />
-                    </div>
-                    <div className="grid gap-2">
-                        <div className="flex items-center">
-                            <Label htmlFor="role">Role</Label>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                                <Link
+                                    href="/404"
+                                    className="ml-auto inline-block text-sm underline"
+                                >
+                                    Forgot your password?
+                                </Link>
+                            </div>
+                            <Input
+                                id="password"
+                                type="password"
+                                name="password"
+                                required
+                            />
                         </div>
-                        <Select name="role" required>
-                            <SelectTrigger className="w-full">
-                                <SelectValue placeholder="Select your account type" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="doctor">Doctor</SelectItem>
-                                <SelectItem value="helper">Helper</SelectItem>
-                            </SelectContent>
-                        </Select>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="role">Role</Label>
+                            </div>
+                            <Select name="role" required>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Select your account type" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="doctor">
+                                        Doctor
+                                    </SelectItem>
+                                    <SelectItem value="helper">
+                                        Helper
+                                    </SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
                     </div>
-                    <Button type="submit" className="w-full">
-                        Login
-                    </Button>
-                    {/* <Button asChild variant="outline" className="w-full">
-                        <Link href="/register">Register</Link>
-                    </Button> */}
-                </div>
-                <div className="mt-4 text-center text-sm">
-                    Don&apos;t have an account?{" "}
-                    <Link href="/register" className="underline">
-                        Register
-                    </Link>
-                </div>
-            </CardContent>
-        </Card>
-        // <Card className="space-y-6 w-full max-w-md bg-accent/20">
-        //     <CardHeader className="space-y-3">
-        //         <h1 className="text-3xl font-semibold text-center">Login</h1>
-        //         <h2 className="text-center">
-        //             New to Mediscript ?{" "}
-        //             <Link
-        //                 href="/register"
-        //                 className="text-primary/80 hover:underline"
-        //             >
-        //                 Sign up
-        //             </Link>{" "}
-        //             here.
-        //         </h2>
-        //     </CardHeader>
-        //     <CardContent>
-        //         <form onSubmit={handleSubmit} className="space-y-4">
-        //             <div className="space-y-2">
-        //                 <Label> Email</Label>
-        //                 <Input
-        //                     type="email"
-        //                     name="email"
-        //                     placeholder="harrypotter@hogwarts.com"
-        //                     required
-        //                 />
-        //             </div>
-        //             <div className="space-y-2">
-        //                 <Label>Password</Label>
-        //                 <Input
-        //                     type="password"
-        //                     name="password"
-        //                     placeholder="Password"
-        //                     required
-        //                 />
-        //             </div>
-        //             <div className="space-y-2">
-        //                 <Label>Role</Label>
-        //                 <Select name="role">
-        //                     <SelectTrigger className="w-full">
-        //                         <SelectValue placeholder="Doctor/Helper" />
-        //                     </SelectTrigger>
-        //                     <SelectContent>
-        //                         <SelectItem value="doctor">Doctor</SelectItem>
-        //                         <SelectItem value="helper">Helper</SelectItem>
-        //                     </SelectContent>
-        //                 </Select>
-        //             </div>
-
-        //             <Button type="submit" className="w-36">
-        //                 Login
-        //             </Button>
-        //         </form>
-        //     </CardContent>
-        // </Card>
+                    <div className="mt-4 text-center text-sm">
+                        Don&apos;t have an account?
+                        <Link href="/register" className="underline">
+                            Register
+                        </Link>
+                    </div>
+                </CardContent>
+            </Card>
+        </form>
     );
 };
 
 export default LoginForm;
-
-// import Link from "next/link"
-
-// import { Button } from "@/components/ui/button"
-import {
-    //   Card,
-    //   CardContent,
-    CardDescription,
-    //   CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
-// import { Input } from "@/components/ui/input"
-// import { Label } from "@/components/ui/label"
-
-const description =
-    "A login form with email and password. There's an option to login with Google and a link to sign up if you don't have an account.";
